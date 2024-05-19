f2f29ed4d26cefa209b0b01a47a03aa7a14df8b9
Favour Adekogbe <favouradekogbe@gmail.com>
2024-05-19 01:26:15 +0100

    hash links

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import React from "react";
 import { Home } from "pages";
+import { useLocation } from "react-router-dom";
 
 function App() {
+  const { pathname, hash } = useLocation();
+
+  React.useEffect(() => {
+    if (hash) {
+      const id = hash.replace("#", "");
+      const element = document.getElementById(id);
+      if (element) {
+        let pos = element.style.position;
+        let top = element.style.top;
+        element.style.position = "relative";
+        element.style.top = "-100px";
+        element.scrollIntoView({ behavior: "smooth", block: "start" });
+        element.style.top = top;
+        element.style.position = pos;
+      }
+    }
+  }, [pathname, hash]);
   return <Home />;
 }
 
